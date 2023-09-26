a7ad34d40330536e775a20f675b0b9ba15597ad3
Rob Brennan <rob@therobbrennan.com>
2023-09-26 00:25:52 -0700

    Attempt to resolve responsive styling

--- apps/nhl-preseason-demo/src/app/page.tsx
+++ apps/nhl-preseason-demo/src/app/page.tsx
@@ -6,23 +6,23 @@ const EmbeddedContent = () => {
   const game2Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010024";
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="relative w-full md:pb-[56.25%] pb-[75%]">
+    <div className="w-full m-0 p-0">
+      <div className="relative w-full md:pb-[56.25%] pb-[75%] m-0">
         <iframe
           src={game1Url}
-          className="absolute top-0 left-0 w-full h-full"
-          frameBorder="0"
+          className="absolute top-0 left-0 w-full h-full m-0 border-0"
           scrolling="no"
           title="Game 1"
+          style={{ width: '100vw' }}
         />
       </div>
-      <div className="relative w-full md:pb-[56.25%] pb-[75%]">
+      <div className="relative w-full md:pb-[56.25%] pb-[75%] m-0">
         <iframe
           src={game2Url}
-          className="absolute top-0 left-0 w-full h-full"
-          frameBorder="0"
+          className="absolute top-0 left-0 w-full h-full m-0 border-0"
           scrolling="no"
           title="Game 2"
+          style={{ width: '100vw' }}
         />
       </div>
     </div>
