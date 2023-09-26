dc2748dcc2d45252e8a3555a5403d17bf1b60a9a
Rob Brennan <rob@therobbrennan.com>
2023-09-25 23:58:11 -0700

    Experiment with md breakpoint styling

--- apps/nhl-preseason-demo/src/app/page.tsx
+++ apps/nhl-preseason-demo/src/app/page.tsx
@@ -5,16 +5,16 @@ const EmbeddedContent = () => {
   const game2Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010024"
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center md:flex-row md:items-start">
       <embed
         src={game1Url}
         type="text/html"
-        className="w-full h-full sm:w-9/10 sm:h-9/10">
+        className="w-full h-[50vh] md:w-[100vw] md:h-[100vh]">
       </embed>
       <embed
         src={game2Url}
         type="text/html"
-        className="w-full h-full sm:w-9/10 sm:h-9/10">
+        className="w-full h-[50vh] md:w-[100vw] md:h-[100vh]">
       </embed>
     </div>
   );
