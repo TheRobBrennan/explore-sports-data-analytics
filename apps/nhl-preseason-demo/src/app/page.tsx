7085cf22e18f158e504c692106e6bc3b59deb789
Rob Brennan <rob@therobbrennan.com>
2023-09-26 00:29:42 -0700

    Remove scrolling attribute

--- apps/nhl-preseason-demo/src/app/page.tsx
+++ apps/nhl-preseason-demo/src/app/page.tsx
@@ -11,18 +11,16 @@ const EmbeddedContent = () => {
         <iframe
           src={game1Url}
           className="absolute top-0 left-0 w-full h-full m-0 border-0"
-          scrolling="no"
           title="Game 1"
-          style={{ width: '90vw' }}
+          style={{ width: '100vw' }}
         />
       </div>
       <div className="relative w-full md:pb-[56.25%] pb-[75%] m-0">
         <iframe
           src={game2Url}
           className="absolute top-0 left-0 w-full h-full m-0 border-0"
-          scrolling="no"
           title="Game 2"
-          style={{ width: '90vw' }}
+          style={{ width: '100vw' }}
         />
       </div>
     </div>
