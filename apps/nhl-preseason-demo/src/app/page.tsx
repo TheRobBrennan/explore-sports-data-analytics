fb44775d5e00e7303d10eda887b5fa8f63bf7bf6
Rob Brennan <rob@therobbrennan.com>
2023-09-25 19:21:29 -0700

    Update page.tsx with 100vw and 100vh

--- apps/nhl-preseason-demo/src/app/page.tsx
+++ apps/nhl-preseason-demo/src/app/page.tsx
@@ -32,16 +32,16 @@ const EmbeddedContent = () => {
         ref={iframeRef1}
         src={game1}
         onLoad={() => handleIframeLoad(iframeRef1)}
-        width="500"
-        height="500"
+        width="100vw"
+        height="100vh"
         title="Game 1"
       />
       <iframe
         ref={iframeRef2}
         src={game2}
         onLoad={() => handleIframeLoad(iframeRef2)}
-        width="500"
-        height="500"
+        width="100vw"
+        height="100vh"
         title="Game 2"
       />
     </div>
