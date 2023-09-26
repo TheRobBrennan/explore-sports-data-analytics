4295f3a493a060c3ead66fdfb0a87348c3c19576
Rob Brennan <rob@therobbrennan.com>
2023-09-26 00:12:36 -0700

    Use CSS and JavaScript to maintain a 16:9 aspect ratio

--- apps/nhl-preseason-demo/src/app/page.tsx
+++ apps/nhl-preseason-demo/src/app/page.tsx
@@ -1,25 +1,39 @@
 'use client';
+import React, { useState, useEffect } from 'react';
 
 const EmbeddedContent = () => {
   const game1Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010022";
   const game2Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010024";
 
+  const [dimensions, setDimensions] = useState({
+    height: window.innerWidth * (9 / 16),
+    width: window.innerWidth
+  });
+
+  const updateDimensions = () => {
+    setDimensions({
+      height: window.innerWidth * (9 / 16),
+      width: window.innerWidth
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
   return (
     <div className="flex flex-col items-center">
-      <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
-        <embed
-          src={game1Url}
-          type="text/html"
-          className="absolute top-0 left-0 w-full h-full"
-        />
-      </div>
-      <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
-        <embed
-          src={game2Url}
-          type="text/html"
-          className="absolute top-0 left-0 w-full h-full"
-        />
-      </div>
+      <embed
+        src={game1Url}
+        type="text/html"
+        style={{ width: dimensions.width, height: dimensions.height }}
+      />
+      <embed
+        src={game2Url}
+        type="text/html"
+        style={{ width: dimensions.width, height: dimensions.height }}
+      />
     </div>
   );
 };
