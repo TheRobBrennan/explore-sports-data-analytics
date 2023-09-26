036e0ec15360051721ccd233440b7b0208f8afd0
Rob Brennan <rob@therobbrennan.com>
2023-09-26 00:20:34 -0700

    Use iframes with scrolling set to no

--- apps/nhl-preseason-demo/src/app/page.tsx
+++ apps/nhl-preseason-demo/src/app/page.tsx
@@ -1,43 +1,30 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const EmbeddedContent = () => {
   const game1Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010022";
   const game2Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010024";
 
-  const [dimensions, setDimensions] = useState({
-    height: 0,
-    width: 0
-  });
-
-  const updateDimensions = () => {
-    setDimensions({
-      height: window.innerWidth * (9 / 16),
-      width: window.innerWidth
-    });
-  };
-
-  useEffect(() => {
-    // Ensure window is defined
-    if (typeof window !== 'undefined') {
-      updateDimensions(); // Initial update
-      window.addEventListener("resize", updateDimensions);
-      return () => window.removeEventListener("resize", updateDimensions);
-    }
-  }, []);
-
   return (
     <div className="flex flex-col items-center">
-      <embed
-        src={game1Url}
-        type="text/html"
-        style={{ width: `${dimensions.width}px`, height: `${dimensions.height}px` }}
-      />
-      <embed
-        src={game2Url}
-        type="text/html"
-        style={{ width: `${dimensions.width}px`, height: `${dimensions.height}px` }}
-      />
+      <div className="relative w-full md:pb-[56.25%] pb-[75%]">
+        <iframe
+          src={game1Url}
+          className="absolute top-0 left-0 w-full h-full"
+          frameBorder="0"
+          scrolling="no"
+          title="Game 1"
+        />
+      </div>
+      <div className="relative w-full md:pb-[56.25%] pb-[75%]">
+        <iframe
+          src={game2Url}
+          className="absolute top-0 left-0 w-full h-full"
+          frameBorder="0"
+          scrolling="no"
+          title="Game 2"
+        />
+      </div>
     </div>
   );
 };
