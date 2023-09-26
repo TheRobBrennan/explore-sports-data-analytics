0454e0d756d4d78f470d50abda80a358b2525b15
Rob Brennan <rob@therobbrennan.com>
2023-09-26 00:01:59 -0700

    Set view and width to 90%

--- apps/nhl-preseason-demo/src/app/page.tsx
+++ apps/nhl-preseason-demo/src/app/page.tsx
@@ -5,16 +5,16 @@ const EmbeddedContent = () => {
   const game2Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010024"
 
   return (
-    <div className="flex flex-col items-center md:flex-row md:items-start">
+    <div className="flex flex-col items-center">
       <embed
         src={game1Url}
         type="text/html"
-        className="w-full h-[50vh] md:w-[100vw] md:h-[100vh]">
+        style={{ width: '90vw', height: '90vh' }}>
       </embed>
       <embed
         src={game2Url}
         type="text/html"
-        className="w-full h-[50vh] md:w-[100vw] md:h-[100vh]">
+        style={{ width: '90vw', height: '90vh' }}>
       </embed>
     </div>
   );
