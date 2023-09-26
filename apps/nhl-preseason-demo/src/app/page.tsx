b1a03f32a9e5ebed75c976f836bb7035f513638e
Rob Brennan <rob@therobbrennan.com>
2023-09-26 00:08:18 -0700

    Improve styling with embeds wrapped in divs

--- apps/nhl-preseason-demo/src/app/page.tsx
+++ apps/nhl-preseason-demo/src/app/page.tsx
@@ -1,21 +1,25 @@
-'use client'
+'use client';
 
 const EmbeddedContent = () => {
-  const game1Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010022"
-  const game2Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010024"
+  const game1Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010022";
+  const game2Url = "https://nhl-shot-chart-on-vercel-with-fastapi.vercel.app/?gameId=2023010024";
 
   return (
     <div className="flex flex-col items-center">
-      <embed
-        src={game1Url}
-        type="text/html"
-        style={{ width: '90vw', height: '90vh' }}>
-      </embed>
-      <embed
-        src={game2Url}
-        type="text/html"
-        style={{ width: '90vw', height: '90vh' }}>
-      </embed>
+      <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
+        <embed
+          src={game1Url}
+          type="text/html"
+          className="absolute top-0 left-0 w-full h-full"
+        />
+      </div>
+      <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
+        <embed
+          src={game2Url}
+          type="text/html"
+          className="absolute top-0 left-0 w-full h-full"
+        />
+      </div>
     </div>
   );
 };
